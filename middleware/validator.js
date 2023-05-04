be27f87aa4681ce67af21763328efac49dd9a16a
Sourav123-sys <khansourav58@gmail.com>
2023-05-05 02:18:23 +0600

    express-validator-add

---
+++ middleware/validator.js
@@ -0,0 +1,20 @@
+const { check,validationResult } = require('express-validator');
+exports.userValidator = [
+    check('name').trim().notEmpty().withMessage("name is invalid"),
+  
+    check('email').trim().normalizeEmail().notEmpty().isEmail().withMessage("email is invalid"),
+    check('password').trim().notEmpty().withMessage("password is missing").isLength({ min:4,max:8}).withMessage("password must be 4 to 8 characters")
+]
+
+exports.validate = (req, res, next) => { 
+    const error = validationResult(req).array();
+    console.log(error);
+//if (!error.isEmpty()) {
+//  console.log(error.array());
+ // return res.status(400).json({ errors: error.array() });
+//}
+    if (error.length) {
+      return  res.json({ error:error[0].msg})
+    }
+    next()
+}
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const { create } = require('../controllers/user');
+const { userValidator, validate } = require('../middleware/validator');
+
 const router = express.Router()
 
 
 
 
 
-router.post("/create", create)
+router.post("/create",userValidator,validate, create)
  
 module.exports = router;
